eb69e9d24bdad051200faa8b901ce3d7c0a674b8
My Name <aakarshpatel84@gmail.com>
2022-11-10 21:50:50 +0530

    db.json Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+// import CaptionCarousel from './components/CaptionCarousel';
+import Navbar from './components/Navbar';
+import AllRouter from './Routes/AllRouter';
 
 function App() {
   return (
     <div className="App">
-    </div>
+      <Navbar />
+      <AllRouter />
+      {/* <CaptionCarousel /> */}
+
+    </div >
   );
 }
 
